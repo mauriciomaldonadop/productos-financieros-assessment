2e350db569327b4eb0f5e728704d04f18f799cde
mauriciomaldonadop@gmail.com <mauriciomaldonadop@gmail.com>
2026-06-03 15:44:58 -0500

    feat(F4): Registration form layout and header refactoring

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductFormComponent } from './components/product-form/product-form.component';
 
 export const routes: Routes = [
   { path: '', component: ProductListComponent },
+  { path: 'add', component: ProductFormComponent },
   { path: '**', redirectTo: '' }
 ];
