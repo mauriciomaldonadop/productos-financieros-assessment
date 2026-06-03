d4a9ac6da5cba9682ea3c05aa510d5b5d9492b8c
mauriciomaldonadop@gmail.com <mauriciomaldonadop@gmail.com>
2026-06-03 12:33:29 -0500

    feat(f1): implement product list UI and integrate with service

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
 import { ApiResponse, FinancialProduct } from '../interfaces/product.interface';
 
