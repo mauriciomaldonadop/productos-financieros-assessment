7e495437cda894513f02ad7a9ea0f9972c36f352
mauriciomaldonadop@gmail.com <mauriciomaldonadop@gmail.com>
2026-06-03 19:43:53 -0500

    feat(F2-f3): Implementation of the search and number of items

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,19 +1,36 @@
-import {Component, inject, signal} from '@angular/core';
+import {Component, computed, inject, signal} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {ProductService} from '../../services/product.service';
+import { ProductService } from '../../services/product.service';
 import {FinancialProduct} from '../../interfaces/product.interface';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
   private productService = inject(ProductService);
   products = signal<FinancialProduct[]>([]);
+  searchTerm = signal<string>('');
+  itemsPerPage = signal<number>(5);
+
+  filteredProducts = computed(() => {
+    const term = this.searchTerm().toLowerCase().trim();
+    if (!term) return this.products();
+
+    return this.products().filter(p =>
+      p.name.toLowerCase().includes(term) ||
+      p.description.toLowerCase().includes(term)
+    );
+  });
+
+  paginatedProducts = computed(() => {
+    return this.filteredProducts().slice(0, this.itemsPerPage());
+  });
 
   ngOnInit(): void {
     this.loadProducts();
